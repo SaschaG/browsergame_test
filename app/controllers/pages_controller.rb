1b2f983f75e2fe9e7d1919caa6bbe68b11b4b73b
Sascha G <sushi.genji@hotmail.de>
2011-06-16 00:06:35 +0200

    Between chapter 3 -4 . Site ATM broken

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+	@title = "Home"
   end
 
   def signin
+  	@title = "Sign in"
   end
 
   def signup
+  	@title = "Sign out"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,39 @@
 require 'spec_helper'
 
 describe PagesController do
+	render_views
+	
+	before(:each) do
+	
+	@base_title = "Browsergame"
+	
+	end
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do
+    	get 'home'
+    	response.should have_selector("title",
+    							:content => @base_title + " | Home")
+	end
   end
+
 
   describe "GET 'signin'" do
     it "should be successful" do
       get 'signin'
       response.should be_success
     end
+    
+    it "should have the right title" do
+    	get 'signin'
+    	response.should have_selector("title",
+    							:content => @base_title + " | Sign in")
+	end
   end
 
   describe "GET 'signup'" do
@@ -21,6 +41,12 @@ describe PagesController do
       get 'signup'
       response.should be_success
     end
+    
+    it "should have the right title" do
+    	get 'signup'
+    	response.should have_selector("title",
+    							:content => @base_title + " | Sign up")
+	end
   end
 
 end
