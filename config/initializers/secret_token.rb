cfc43cb4c361c91a6fc485efe0efb095853361d4
Sascha G <sushi.genji@hotmail.de>
2011-06-15 23:29:45 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrowsergameTest::Application.config.secret_token = 'aa6558a9c9e1a480e5120a3f1d2d7d516ceb19aec0ba3770590a9eb21543989bf703f90825ffe2ef5911a3c9cbfa40266e3fc998652fc0da52a29322708a0298'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BrowsergameTest::Application.config.session_store :cookie_store, :key => '_Browsergame_test_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BrowsergameTest::Application.config.session_store :active_record_store
